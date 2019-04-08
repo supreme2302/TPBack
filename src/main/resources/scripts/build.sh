dbd40ef898b8f18f78936314aaed85a8df551b6c
BritikovKI <ShermanCrab1>
2019-04-09 01:36:15 +0300

    build fix

--- src/main/resources/scripts/build.sh
+++ src/main/resources/scripts/build.sh
@@ -2,9 +2,10 @@
 # 1 - schoolID, 2 - mainColor, 3 - secondaryColor
 # 4 - name, 5 - language
 files_upload_dir="src/main/resources/applications"
-flavors_dir="../lingvomake-android/app/src/main/res/values/"
-android_dir="../lingvomake-android"
-apk_dir="../lingvomake-android/app/build/outputs/apk/debug"
+flavors_dir="../Lingvomake/app/src/main/res/values/"
+android_dir="../Lingvomake"
+back_dir="../TPBack"
+apk_dir="../Lingvomake/app/build/outputs/apk/debug"
 scripts_dir="src/main/resources/scripts"
 set_tag_value="$scripts_dir/set_xml_tag_value.sh"
 app_suffix=whitelabel$1dc
@@ -17,9 +18,11 @@ source "$set_tag_value" "$flavors_dir"colors.xml color colorPrimary "$2"
 source "$set_tag_value" "$flavors_dir"colors.xml color colorPrimaryDark "$3"
 source "$set_tag_value" "$flavors_dir"strings.xml string app_name "$4"
 ## Build
+cd $android_dir
+./gradlew assembleDebug -q
+cd $back_dir
 
-$android_dir/gradlew assembleDebug -q
-mv $apk_dir/app-debug.apk $files_upload_dir/$1.apk
+mv $apk_dir/app-debug.rapk $files_upload_dir/$1.apk
 
 #
 ## Clean and copy APK
