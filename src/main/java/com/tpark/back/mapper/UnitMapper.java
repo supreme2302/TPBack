8ddc8e692c672480fee4009cd30c86a95efc0471
supreme2302 <chuvashov.egor@mail.ru>
2019-05-05 22:43:05 +0300

    domain layer

--- src/main/java/com/tpark/back/mapper/UnitMapper.java
+++ src/main/java/com/tpark/back/mapper/UnitMapper.java
@@ -1,5 +1,6 @@
 package com.tpark.back.mapper;
 
+import com.tpark.back.model.domain.UnitDomain;
 import com.tpark.back.model.dto.UnitDTO;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -8,16 +9,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Component
-public class UnitMapper implements RowMapper<UnitDTO> {
+public class UnitMapper implements RowMapper<UnitDomain> {
     @Override
-    public UnitDTO mapRow(ResultSet resultSet, int i) throws SQLException {
-        UnitDTO unitDTO = new UnitDTO();
-        unitDTO.setId(resultSet.getInt("id"));
-        unitDTO.setCourse_id(resultSet.getInt("course_id"));
-        unitDTO.setUnit_name(resultSet.getString("unit_name"));
-        unitDTO.setPrev_pos(resultSet.getInt("prev_unit"));
-        unitDTO.setNext_pos(resultSet.getInt("next_unit"));
-        unitDTO.setDescription(resultSet.getString("description"));
-        return unitDTO;
+    public UnitDomain mapRow(ResultSet resultSet, int i) throws SQLException {
+        return UnitDomain.builder()
+                .id(resultSet.getInt("id"))
+                .course_id(resultSet.getInt("course_id"))
+                .unit_name(resultSet.getString("unit_name"))
+                .prev_pos(resultSet.getInt("prev_unit"))
+                .next_pos(resultSet.getInt("next_unit"))
+                .description(resultSet.getString("description"))
+                .tags(resultSet.getString("tags"))
+                .build();
     }
 }
--- src/main/java/com/tpark/back/service/Impl/UnitServiceImpl.java
+++ src/main/java/com/tpark/back/service/Impl/UnitServiceImpl.java
@@ -1,11 +1,15 @@
 package com.tpark.back.service.Impl;
 
-import com.tpark.back.dao.Impl.UnitDAOImpl;
 import com.tpark.back.dao.UnitDAO;
+import com.tpark.back.model.domain.UnitDomain;
 import com.tpark.back.model.dto.UnitDTO;
 import com.tpark.back.service.UnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -19,23 +23,24 @@ public class UnitServiceImpl implements UnitService {
     }
 
     @Override
-    public Object getUnitsByCourse(Integer courseId, String email) {
-        return unitDAO.getUnitsByCourse(courseId, email);
+    public List<UnitDTO> getUnitsByCourse(Integer courseId, String email) {
+        return unitDAO.getUnitsByCourse(courseId, email)
+                .stream().map(this::unitDomainToDTO).collect(Collectors.toList());
     }
 
     @Override
-    public Object getUnit(Integer groupId, String email) {
-        return unitDAO.getUnit(groupId, email);
+    public UnitDTO getUnit(Integer groupId, String email) {
+        return unitDomainToDTO(unitDAO.getUnit(groupId, email));
     }
 
     @Override
     public void createUnit(UnitDTO unitDTO, String email) {
-        unitDAO.createUnit(unitDTO, email);
+        unitDAO.createUnit(unitDTOToDomain(unitDTO), email);
     }
 
     @Override
     public void changeUnit(UnitDTO unitDTO, String email) {
-        unitDAO.changeUnit(unitDTO, email);
+        unitDAO.changeUnit(unitDTOToDomain(unitDTO), email);
     }
 
     @Override
@@ -44,24 +49,51 @@ public class UnitServiceImpl implements UnitService {
     }
 
     @Override
-    public Object getUnitForStudent(Integer unitId, String student) {
-        return unitDAO.getUnitForStudent(unitId, student);
+    public UnitDTO getUnitForStudent(Integer unitId, String student) {
+        return unitDomainToDTO(unitDAO.getUnitForStudent(unitId, student));
     }
 
     @Override
-    public Object getUnitsByCourseForStudent(Integer courseId, String student) {
-        return unitDAO.getUnitByCourseForStudent(courseId, student);
+    public List<UnitDTO> getUnitsByCourseForStudent(Integer courseId, String student) {
+        return unitDAO.getUnitByCourseForStudent(courseId, student)
+                .stream().map(this::unitDomainToDTO).collect(Collectors.toList());
     }
 
     @Override
-    public Object getAllUnits(String user) {
-        return unitDAO.getUnits( user);
+    public List<UnitDTO> getAllUnits(String user) {
+        return unitDAO.getUnits(user)
+                .stream().map(this::unitDomainToDTO).collect(Collectors.toList());
     }
 
     @Override
-    public Object getAllUnitsForStudent(String student) {
-        return unitDAO.getUnitsForStudent(student);
+    public List<UnitDTO> getAllUnitsForStudent(String student) {
+        return unitDAO.getUnitsForStudent(student)
+                .stream().map(this::unitDomainToDTO).collect(Collectors.toList());
     }
 
+    private UnitDTO unitDomainToDTO(UnitDomain unitDomain) {
+        UnitDTO unitDTO = new UnitDTO();
+        unitDTO.setId(unitDomain.getId());
+        unitDTO.setUnit_name(unitDomain.getUnit_name());
+        unitDTO.setCourse_id(unitDomain.getCourse_id());
+        unitDTO.setDescription(unitDomain.getDescription());
+        unitDTO.setPrev_pos(unitDomain.getPrev_pos());
+        unitDTO.setNext_pos(unitDomain.getNext_pos());
+        unitDTO.setTags(unitDomain.getTags() == null ? null :
+                Arrays.asList(unitDomain.getTags().split(",")));
+        return unitDTO;
+    }
 
+    private UnitDomain unitDTOToDomain(UnitDTO unitDTO) {
+        return UnitDomain.builder()
+                .id(unitDTO.getId())
+                .course_id(unitDTO.getCourse_id())
+                .description(unitDTO.getDescription())
+                .next_pos(unitDTO.getNext_pos())
+                .prev_pos(unitDTO.getPrev_pos())
+                .unit_name(unitDTO.getUnit_name())
+                .tags(unitDTO.getTags().isEmpty() ? null : String.join(",",
+                        unitDTO.getTags().stream().limit(3).collect(Collectors.toList())))
+                .build();
+    }
 }
